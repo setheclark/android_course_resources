d58cbb6c854de6c57b4b0179b0892074e225bba2
Seth Clark <sethedwardclark@gmail.com>
2018-11-15 17:09:35 -0500

    Day 9 - Pre class setup

---
+++ class/Day9 - Networking_ImageLoading/app/src/main/java/io/sethclark/networkingclasscode/RandomDogActivity.kt
@@ -0,0 +1,12 @@
+package io.sethclark.networkingclasscode
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomDogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_dog)
+    }
+}
